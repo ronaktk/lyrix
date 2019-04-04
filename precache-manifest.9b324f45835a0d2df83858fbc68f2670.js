d6a5b23770596170cf78c922b72bb580820306ca
Ronak Patel <ronaktk@gmail.com>
2019-04-04 16:02:25 -0500

    Updates

---
+++ precache-manifest.9b324f45835a0d2df83858fbc68f2670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "954b014053d149c464b9c8649cea659c",
+    "url": "/lyrix/static/media/spinner.954b0140.gif"
+  },
+  {
+    "revision": "e9cfab8e4fe38846c8c1",
+    "url": "/lyrix/static/js/runtime~main.2aeb6f7a.js"
+  },
+  {
+    "revision": "31e4d0b7cc7c09d55b15",
+    "url": "/lyrix/static/js/main.72a0a051.chunk.js"
+  },
+  {
+    "revision": "3665ca05ff20c2b0acaa",
+    "url": "/lyrix/static/js/2.2f114a51.chunk.js"
+  },
+  {
+    "revision": "31e4d0b7cc7c09d55b15",
+    "url": "/lyrix/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "fdd27e3b491b86c2e261b62a0b9c4681",
+    "url": "/lyrix/index.html"
+  }
+];
